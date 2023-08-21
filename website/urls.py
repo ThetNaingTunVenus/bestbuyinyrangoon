4d1a2cf420f9cd53a1368b62eb852153c37d4baf
kothet <kothetnaingtun.ict@gmail.com>
2023-08-21 21:43:07 +0630

    add website static

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-app_name = 'barcode'
+from .views import *
+from . import views
+app_name = 'shop'
 urlpatterns = [
-    # path(''),
+    path('', TestView.as_view()),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.views.generic import View
+
+from myapp.models import *
 
 # Create your views here.
+class TestView(View):
+    def get(self,request):
+        it = Items.objects.all()
+        context = {'it':it}
+        return render(request,'shop/test.html',context)
